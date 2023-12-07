3be006258d5fece42a9b93cd32d8d531574c7903
WoeunJung <leena09123@naver.com>
2023-12-07 11:29:49 +0900

    feat: add all screens and navigation

---
+++ Green-gift-participant/app/src/main/java/com/greengift/participant/presentation/navigation/Screen.kt
@@ -0,0 +1,11 @@
+package com.greengift.participant.presentation.navigation
+
+sealed class Screen (val route: String){
+    object LoginScreen: Screen("login_screen")
+    object SplashScreen: Screen("splash_screen")
+    object SignupScreen: Screen("sign_up_screen")
+    object FestivalScreen: Screen("festival_screen")
+    object FestivalResultScreen: Screen("festival_result_screen")
+    object GiftScreen: Screen("gift_screen")
+    object MyGiftScreen: Screen("my_gift_screen")
+}
---
+++ Green-gift-participant/app/src/main/java/com/greengift/participant/presentation/view/my_gift/MyGiftScreen.kt
@@ -0,0 +1,11 @@
+package com.greengift.participant.presentation.view.my_gift
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MyGiftScreen(
+    navController: NavController
+){
+    
+}
