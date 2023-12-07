2462285ec7a7fcdacd2e6f95d61b2e08af43e597
WoeunJung <leena09123@naver.com>
2023-12-07 11:26:28 +0900

    chore:  add serialization plugins

--- Green-gift-participant/build.gradle.kts
+++ Green-gift-participant/build.gradle.kts
@@ -1,5 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript{
     repositories {
         google()
@@ -16,5 +14,4 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
 }
